985f08441794948c497fc3561316dea85fe87a8c
RigbyDawg <rb22240301@bachilleresdesonora.edu.mx>
2025-05-17 10:29:02 -0700

    Create Java.js

---
+++ Java.js
@@ -0,0 +1,1 @@
+alert('Bienvenido a nuestra pagina de fans para fans de metallica!!!, aqui encontraras todo tipo de informacion, ya sea biografias, discografias, etc. Esperemos que te guste mucho :D')
