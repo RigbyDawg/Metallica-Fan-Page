f5210825b076546bd7188015e02c9904ec027015
RigbyDawg <rb22240301@bachilleresdesonora.edu.mx>
2025-05-19 23:18:08 -0700

    Add files via upload

--- Java.js
+++ Java.js
@@ -1,1 +1,38 @@
-alert('Bienvenido a nuestra pagina de fans para fans de metallica!!!, aqui encontraras todo tipo de informacion, ya sea biografias, discografias, etc. Esperemos que te guste mucho :D')
+const dropdown = document.querySelectorAll('.dropdown');
+dropdown.forEach(dropdown => {
+     const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const optiones = dropdown.querySelector('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+select.addEventListener('click', () => {
+    select.classList.toggle('select-clicked');
+    caret.classList.toggle('caret-rotate');
+    menu.classList.toggle('menu-open');
+
+
+});
+
+optiones.forEach(option => {
+
+    optiones.addEventListener('click', () => {
+
+       select.innerText = option.innerText;
+       select.classList.remove('select-clicked');
+       caret.classList.remove('caret-rotate');
+       menu.classList.remove('menu-open');
+       optiones.forEach(option => {
+option.classList.remove('active');
+
+       });
+    });
+});
+
+
+
+
+
+
+})
+   
